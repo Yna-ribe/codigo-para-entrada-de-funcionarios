9423d5462731658feeaee458df534a63ede026f3
Yna-ribe <ynae.ribeiro@outlook.com>
2024-11-07 09:48:06 -0300

    mudança de acesso do cargo

--- acesso_livre_do_gerente.py
+++ acesso_livre_do_gerente.py
@@ -1,8 +1,8 @@
-print('Sistema de controle de acesso do escritório para: gerentes, estagiario e secretario')
+print('Sistema de controle de acesso do escritório para: gerentes, estagiario e analista')
 cargo_do_funcionario=input('qual seu cargo? ')
 dia_da_semana=input('qual o dia da semana? ')
 
-while(cargo_do_funcionario != "gerente" or "estagiario" or "secretario"):
+while(cargo_do_funcionario != "gerente" or "estagiario" or "analista"):
     for i in range(3):
         print('acesso negado, tente outra vez, você só tem {i}tentativas')
 
