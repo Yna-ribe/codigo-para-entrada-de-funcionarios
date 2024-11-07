af20ec259dade93030a5ba6a37c14fcfd60e5795
Yna-ribe <ynae.ribeiro@outlook.com>
2024-11-07 09:45:38 -0300

    verificação aprimorada

--- acesso_livre_do_gerente.py
+++ acesso_livre_do_gerente.py
@@ -1,9 +1,11 @@
 print('Sistema de controle de acesso do escritório para: gerentes, estagiario e secretario')
 cargo_do_funcionario=input('qual seu cargo? ')
 dia_da_semana=input('qual o dia da semana? ')
-for i in range(3):
-    while(cargo_do_funcionario != "gerente" or "estagiario" or "secretario"):
-        print('acesso negado, tente outra vez, você só tem {i}tentativas'
+
+while(cargo_do_funcionario != "gerente" or "estagiario" or "secretario"):
+    for i in range(3):
+        print('acesso negado, tente outra vez, você só tem {i}tentativas')
+
 if(cargo_do_funcionario == 'gerente'):
     print ('Acesso permitido',cargo_do_funcionario)
 elif(dia_da_semana == 'sabado' or dia_da_semana == 'domingo'):
